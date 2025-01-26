21e402b86a4ba43dd6a78704def4d62c0019d3ea
MonoLogueChi <xxwhite@foxmail.com>
2025-01-26 22:30:42 +0800

    修改单例模式方法

--- Assets/Scripts/Sb/UnityKit/Singleton/MonoSingleton.cs
+++ Assets/Scripts/Sb/UnityKit/Singleton/MonoSingleton.cs
@@ -1,55 +1,72 @@
+using Cysharp.Threading.Tasks;
 using UnityEngine;
 
-namespace Sb.UnityKit.Singleton
+namespace Sb.UnityKit.Singleton;
+
+public abstract class MonoSingleton<T> : MonoBehaviour, ISingleton<T> where T : MonoBehaviour
 {
-  public class MonoSingleton<T> : MonoBehaviour, ISingleton<T> where T : MonoBehaviour
+  private static T _instance = null!;
+
+  // ReSharper disable once InconsistentNaming
+  // ReSharper disable once StaticMemberInGenericType
+  private static readonly object _lock = new();
+
+  // ReSharper disable once StaticMemberInGenericType
+  private static bool _applicationIsQuitting;
+
+  protected virtual bool DontDestroyInstanceOnLoad { get; } = false;
+
+  public static T Instance
   {
-    private static T _instance = null!;
-
-    // ReSharper disable once InconsistentNaming
-    // ReSharper disable once StaticMemberInGenericType
-    private static readonly object _lock = new();
-
-    // ReSharper disable once StaticMemberInGenericType
-    private static bool _applicationIsQuitting;
-
-    public static T Instance
+    get
     {
-      get
+      lock (_lock)
       {
-        lock (_lock)
+        if (_instance) return _instance;
+
+        _instance = FindAnyObjectByType<T>();
+
+        if (_instance) return _instance;
+
+        var singleton = new GameObject
         {
-          if (_instance == null)
-          {
-            _instance = (T)FindObjectOfType(typeof(T));
+          name = "(singleton) " + typeof(T)
+        };
+        _instance = singleton.AddComponent<T>();
 
-            if (FindObjectsOfType(typeof(T)).Length > 1) return _instance;
-
-            if (_instance == null)
-            {
-              var singleton = new GameObject()
-              {
-                name = "(singleton) " + typeof(T)
-              };
-              _instance = singleton.AddComponent<T>();
-
-              DontDestroyOnLoad(singleton);
-            }
-          }
-
-          return _instance;
-        }
+        return _instance;
       }
     }
+  }
 
-    public void OnDestroy()
-    {
-      _applicationIsQuitting = true;
-    }
+  // ReSharper disable once Unity.IncorrectMethodSignature
+#pragma warning disable UNT0006
+  private async UniTaskVoid Awake()
+#pragma warning restore UNT0006
+  {
+    if (DontDestroyInstanceOnLoad) DontDestroyOnLoad(this);
+    // ReSharper disable once MethodHasAsyncOverload
+    OnAwake();
+    await OnAwakeAsync();
+  }
 
-    public static bool IsDestroy()
-    {
-      return _applicationIsQuitting;
-    }
+
+  public void OnDestroy()
+  {
+    _applicationIsQuitting = true;
+  }
+
+  protected virtual void OnAwake()
+  {
+  }
+
+  protected virtual UniTask OnAwakeAsync()
+  {
+    return UniTask.CompletedTask;
+  }
+
+  public static bool IsDestroy()
+  {
+    return _applicationIsQuitting;
   }
 }
